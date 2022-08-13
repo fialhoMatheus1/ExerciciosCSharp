f74bafc8b3251a98d8c5d447243f4d0adeeeda95
fialhoMatheus1 <104318023+fialhoMatheus1@users.noreply.github.com>
2022-08-12 22:39:58 -0300

    Add files via upload

---
+++ ExerciciosTI14T2/ExerciciosTI14T2/ExerciciosTI14T/Control.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI14T
+{
+    class ControlExercicios
+    {
+        //Ligar as duas classes
+        ModelExercicios model;
+        private int opcao;
+        public double bas;
+        public double altura;
+        public double maca;
+        public double fixo;
+        public double vendas;
+        public int ano;
+        public int meses;
+        public int dias;
+        public double eleitores;
+        public double brancos;
+        public double nulos;
+        public double validos;
+        public double salario;
+        public double reajuste;
+        public double custoDeFabrica;
+        public int numConta;
+
+        public ControlExercicios()
+        {
+            model = new ModelExercicios();//Chamando o construtor da classe Model Exercicios
+        }//fim do construtor
+
+        public int ConsultarOpcao
+        {
+            get
+            {
+                return opcao;
+            }
+            set
+            {
+                this.opcao = value;
+            }
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("---- Menu ----" +
+                             "\n1. Exercício 01" +
+                             "\n2. Exercício 02" +
+                             "\n3. Exercício 03" +
+                             "\n4. Exercício 04" +
+                             "\n5. Exercício 05" +
+                             "\n6. Exercício 06" +
+                             "\n7. Exercício 07" +
+                             "\n8. Exercício 08" +
+                             "\n9. Exercício 09" +
+                             "\n11. Exercicio 11" +
+                             "\n12. Exercicio 12" +
+                             "\n0. Sair");
+            Console.WriteLine("Escolha uma das opções acima: ");
+            ConsultarOpcao = Convert.ToInt32(Console.ReadLine());
+        }//fim do menu
+
+        public void Executar()
+        {
+            Menu();
+            //Executar a ação
+            switch (ConsultarOpcao)
+            {
+                case 1:
+                    Console.WriteLine(model.Exercicio01());
+                    break;
+
+                case 2:
+                    Console.WriteLine("Informe um número");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("O antecessor de " + num +
+                        " é: " + model.Exercicio02(num));
+                    break;
+
+                case 3:
+                    do
+                    {
+                        Console.WriteLine("Informe a base: ");
+                        bas = Convert.ToDouble(Console.ReadLine());
+                        if (bas <= 0)
+                        {
+                            Console.WriteLine("Base digitada não é valida," +
+                                " digite novamente!");
+                        }
+                    } while (bas <= 0);
+
+                    do
+                    {
+                        Console.WriteLine("Informe a altura: ");
+                        altura = Convert.ToDouble(Console.ReadLine());
+                        if (altura <= 0)
+                        {
+                            Console.WriteLine("Altura digitada não é valida," +
+                                " digite novamente!");
+                        }
+                    } while (altura <= 0);
+
+                    Console.WriteLine("A área do retângulo é: " +
+                        model.Exercicio03(bas, altura));
+                    break;
+
+                case 4:
+                    do
+                    {
+                        Console.WriteLine("Informe quantos anos você tem: ");
+                        ano = Convert.ToInt32(Console.ReadLine());
+                        if (ano <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (ano <= 0);
+                    do
+                    {
+                        Console.WriteLine("Informe quantos meses você tem: ");
+                        meses = Convert.ToInt32(Console.ReadLine());
+                        if (meses <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (meses < 0);
+                    do
+                    {
+                        Console.WriteLine("Informe quantos dias vocês tem: ");
+                        dias = Convert.ToInt32(Console.ReadLine());
+                        if (dias <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (dias <= 0);
+                    Console.WriteLine("Sua idade em dias é: " + model.Exercicio04(ano, meses, dias) + " dias");
+                    break;
+
+                case 5:
+                    do
+                    {
+                        Console.WriteLine("Informe o total de eleitores: ");
+                        eleitores = Convert.ToDouble(Console.ReadLine());
+                        if (eleitores <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (eleitores <= 0);
+                    do
+                    {
+                        Console.WriteLine("Informe o total de brancos: ");
+                        brancos = Convert.ToDouble(Console.ReadLine());
+                        if (brancos <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (brancos <= 0);
+                    do
+                    {
+                        Console.WriteLine("Informe o total de nulos: ");
+                        nulos = Convert.ToDouble(Console.ReadLine());
+                        if (nulos <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (nulos <= 0);
+                    do
+                    {
+                        Console.WriteLine("Informe o total de válidos: ");
+                        validos = Convert.ToDouble(Console.ReadLine());
+                        if (validos <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (validos <= 0);
+                    Console.WriteLine(model.Exercicio05(eleitores, brancos, nulos, validos));
+                    break;
+
+                case 6:
+                    do
+                    {
+                        Console.WriteLine("Digite o seu salario: ");
+                        salario = Convert.ToDouble(Console.ReadLine());
+                        if (salario < 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (salario <= 0);
+                    do
+                    {
+                        Console.WriteLine("Digite o reajuste: ");
+                        reajuste = Convert.ToDouble(Console.ReadLine());
+                        if (reajuste <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (reajuste <= 0);
+                    Console.WriteLine("O seu novo salário é:" + model.Exercicio06(salario, reajuste));
+                    break;
+
+                case 7:
+                    do
+                    {
+                        Console.WriteLine("Digite o custo de fabrica do carro: ");
+                        custoDeFabrica = Convert.ToDouble(Console.ReadLine());
+                        if (custoDeFabrica <= 0)
+                        {
+                            Console.WriteLine("Valor inválido." +
+                                "\nTente novamente.");
+                        }
+                    } while (custoDeFabrica <= 0);
+                    Console.WriteLine("O custo final ao consumidor é: R$ " + model.Exercicio07(custoDeFabrica));
+                    break;
+                    
+                case 8:
+                    Console.WriteLine("Digite sua primeira nota: ");
+                    double nota1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite sua segunda nota: ");
+                    double nota2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite sua terceira nota: ");
+                    double nota3 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Sua média é: " + model.Exercicio08(nota1, nota2, nota3));
+                    break;
+
+                case 9:
+                    do
+                    {
+                        Console.WriteLine("Digite o número de maçãs compradas: ");
+                        maca = Convert.ToDouble(Console.ReadLine());
+                        if (maca < 0)
+                        {
+                            Console.WriteLine("O número digitado é invalido." +
+                                "\nTente novamente.");
+                        }
+                    } while (maca < 0);
+
+                    Console.WriteLine("O total a ser pago pelas maças é: R$ " + model.Exercicio09(maca) + ",00");
+                    break;
+
+                //exercicio10
+
+                case 11:
+                    do
+                    {
+                        Console.WriteLine("Digite o seu salário fixo: ");
+                        fixo = Convert.ToDouble(Console.ReadLine());
+                        if (fixo <= 0)
+                        {
+                            Console.WriteLine("Valor digitado é invalido." +
+                                "\nTente novamente.");
+                        }
+                    } while (fixo <= 0);
+                    do
+                    {
+                        Console.WriteLine("Digite o valor total de vendas efetuadas");
+                        vendas = Convert.ToDouble(Console.ReadLine());
+                        if (vendas <= 0)
+                        {
+                            Console.WriteLine("Valor digitado é invalido." +
+                                "\nTente novamente.");
+                        }
+                    } while (vendas <= 0);
+                    Console.WriteLine("Seu salário total é: R$" + model.Exercicio11(fixo, vendas));
+                    break;
+
+                case 12:
+                    do
+                    {
+                        Console.WriteLine("Digite o numero da sua conta bancaria");
+                        numConta = Convert.ToInt32(Console.ReadLine());
+                        if (numConta <= 0)
+                        {
+                            Console.WriteLine("Valor digitado é invalido." +
+                                "\nTente novamente.");
+                        }
+                    } while (numConta <= 0);
+                    Console.WriteLine("Digite o seu saldo");
+                    int saldo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite seu valor de débito");
+                    int debito = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite seu valor de credito");
+                    int credito = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Seu saldo atual é: R$ " + model.Exercicio12(numConta, saldo, debito, credito));
+                    break;
+
+                case 0:
+                    Console.WriteLine("Obrigado!!");
+                    break;
+
+                default:
+                    Console.WriteLine("Código digitado não é valido!");
+                    break;
+            }//fim do switch
+        }//fim do executar
+    }//fim da classe
+}//fim do projeto
---
+++ ExerciciosTI14T2/ExerciciosTI14T2/ExerciciosTI14T/Model.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI14T
+{
+    class ModelExercicios
+    {
+        //Declaração de variáveis
+        private int A;
+        private int B;
+        private int auxiliar;
+        private double eleitores;
+        private double brancos;
+        private double nulos;
+        private double validos;
+        private double reajuste;
+        private double aux;
+
+        //Declarar o construtor
+        public ModelExercicios()
+        {
+            ConsultarA = 10;
+            ConsultarB = 20;
+            ConsultarAuxiliar = 0;
+            ConsultarEleitores = 0;
+            ConsultarBrancos = 0;
+            ConsultarNulos = 0;
+            ConsultarValidos = 0;
+            ConsultarReajuste = 0;
+            ConsultarAux = 0;
+            ConsultarNotas = 0;
+            ConsultarMaca = 0;
+            ConsultarCusto = 0;
+            ConsultarVendas = 0;
+            ConsultarSaldo = 0;
+            ConsultarCalculadora = 0;
+        }//fim do construtor
+
+        public int ConsultarA
+        {
+            get
+            {
+                return A;
+            }
+            set
+            {
+                this.A = value;
+            }
+        }//fim do consultarA
+
+        public int ConsultarB
+        {
+            get
+            {
+                return B;
+            }
+            set
+            {
+                this.B = value;
+            }
+        }//fim do consultarB
+
+        public int ConsultarAuxiliar
+        {
+            get
+            {
+                return auxiliar;
+            }
+            set
+            {
+                this.auxiliar = value;
+            }
+        }//fim do consultarC
+
+        public double ConsultarEleitores
+        {
+            get
+            {
+                return eleitores;
+            }
+            set
+            {
+                this.eleitores = value;
+            }
+        }// fim do consultarEleitores
+
+        public double ConsultarBrancos
+        {
+            get
+            {
+                return brancos;
+            }
+            set
+            {
+                this.brancos = value;
+            }
+        }// fim do consultarBrancos
+
+        public double ConsultarNulos
+        {
+            get
+            {
+                return nulos;
+            }
+            set
+            {
+                this.nulos = value;
+            }
+        }// fim do ConsultarNulos
+
+        public double ConsultarValidos
+        {
+            get
+            {
+                return validos;
+            }
+            set
+            {
+                this.validos = value;
+            }
+        }// fim do ConsultarValidos
+
+        public double ConsultarReajuste
+        {
+            get
+            {
+                return reajuste;
+            }
+            set
+            {
+                this.reajuste = value;
+            }
+        }// fim do metodo consultarPercent
+
+        public double ConsultarAux
+        {
+            get
+            {
+                return aux;
+            }
+            set
+            {
+                this.aux = value;
+            }
+        }// fim do metodo ConsultarAux
+
+        public double ConsultarNotas
+        {
+            get
+            {
+                return aux;
+            }
+            set
+            {
+                this.aux = value;
+            }
+        }// fim do consultarNotas
+
+        public double ConsultarMaca
+        {
+            get
+            {
+                return aux;
+            }
+            set
+            {
+                this.aux = value;
+            }
+        }//fim do consultarMaca
+
+        public double ConsultarCusto
+        {
+            get
+            {
+                return aux;
+            }
+            set
+            {
+                this.aux = value;
+            }
+        }//fim do metodo CustoConsumidor
+
+        public double ConsultarVendas
+        {
+            get
+            {
+                return aux;
+            }
+            set
+            {
+                this.aux = value;
+            }
+        }// fim do metodo consultarVendas
+
+        public int ConsultarSaldo
+        {
+            get
+            {
+                return auxiliar;
+            }
+            set
+            {
+                this.auxiliar = value;
+            }
+        }//fim do metodo consultarSaldo
+
+        public int ConsultarCalculadora
+        {
+            get
+            {
+                return A;
+            }
+            set
+            {
+                this.A = value;
+            }
+        }//fim do metodo consultarCalculadora
+
+        //Exercício01
+
+        public string Exercicio01()
+        {
+            ConsultarAuxiliar = ConsultarA;
+            ConsultarA = ConsultarB;
+            ConsultarB = ConsultarAuxiliar;
+            return "A: " + ConsultarA + "\nB: " + ConsultarB;
+        }//fim do métodoExercicio01
+
+        //Exercicio 02
+
+        public int Exercicio02(int num)
+        {
+            return num - 1;
+        }
+
+        //Exercicio 03
+
+        public double Exercicio03(double bas, double altura)
+        {
+            return bas * altura;
+        }// fim do metodo exercicio03
+
+        //Exercicio 04
+
+        public int Exercicio04(int ano, int mes, int dias)
+        {
+            return (ano * 365) + (mes * 30) + dias;
+        }//fim do método exercicio04
+
+        //Exercicio 05
+
+        public string Exercicio05(double eleitores, double brancos, double nulos, double validos)
+        {
+            ConsultarEleitores = eleitores;
+            ConsultarBrancos = (brancos * ConsultarEleitores) / 100;
+            ConsultarNulos = (nulos * ConsultarEleitores) / 100;
+            ConsultarValidos = (validos * ConsultarEleitores) / 100;
+            return "O percentual de brancos nulos é: " + ConsultarBrancos + "%" +
+                "\nO percentual de nulos é: " + ConsultarNulos + "%" +
+                "\nO perncentual de válidos é: " + ConsultarValidos + "%";
+        }//fim do método exercicios05
+
+        //Exercicio 06
+
+        public double Exercicio06(double salario, double reajuste)
+        {
+            ConsultarReajuste = (salario * reajuste) / 100;
+            ConsultarAux = ConsultarReajuste + salario;
+            return ConsultarAux;
+        }
+
+        //Exercicio 07
+
+        public double Exercicio07(double custoDeFabrica)
+        {
+            ConsultarCusto = custoDeFabrica + (custoDeFabrica * 73) / 100;
+            return ConsultarCusto;
+        }// fim do metodo exercicio07
+
+        //Exercicio 08
+
+        public double Exercicio08(double nota1, double nota2, double nota3)
+        {
+            ConsultarNotas = (nota1 + nota2 + nota3) / 100;
+            return ConsultarNotas;
+        }// fim do metodo exercicio08
+
+        //Exercicio 09
+
+        public double Exercicio09(double maca)
+        {
+            if (maca < 12)
+            {
+                ConsultarMaca = maca * 1.30;
+            }
+            if (maca > 11)
+            {
+                ConsultarMaca = maca * 1.00;
+            }
+            return ConsultarMaca;
+        }// fim do metodo exercio09
+
+        //Exercicio 10
+
+        //Exercicio 11
+
+        public double Exercicio11(double fixo, double vendas)
+        {
+            if(vendas <= 1500)
+            {
+                ConsultarVendas = (vendas * 3) / 100;
+            }
+           if(vendas > 1500)
+            {
+                ConsultarVendas = ((1500 * 3) / 100) + ((vendas - 1500) * 5) / 100;
+                //vendedor recebe os 3% de 1500 vendas, junto com 5% do que vendeu acima de 1500.
+            }
+            return fixo + ConsultarVendas;
+        }//fim do metodo exercicio11
+
+        //Exercicio 12
+
+        public string Exercicio12(int numConta, int saldo, int debito, int credito)
+        {
+            ConsultarSaldo = saldo - debito + credito;
+            if (ConsultarSaldo < 0)
+            {
+                return "Seu salto atual é: R$ " + ConsultarSaldo +
+                    "\nSaldo NEGATIVO!!";
+            }
+            else
+                return "Seu saldo atual é: R$ " + ConsultarSaldo +
+                        "\nSalto POSITIVO!!";
+        }//fim do metodo exercicio12
+
+    }//fim da classe
+}//fim do projeto
